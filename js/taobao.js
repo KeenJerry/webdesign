8a2d61e9a0b6b16426dfbdce091f1181c123d906
ZhouKan <3286307856@qq.com>
2018-04-19 15:52:21 +0800

    new changes

--- js/taobao.js
+++ js/taobao.js
@@ -1,6 +1,58 @@
-
-
 $(".searcher-tab").click(function(){
     $(".searcher-tab.searcher-tab-is-active").toggleClass("searcher-tab-is-active");
     $(this).toggleClass("searcher-tab-is-active");
 })
+
+// slider 1
+var range_1 = -520;
+setInterval(function(){
+    $(".slider-1").css("transform", "translate3d(" + range_1 + "px,0px,0px)");
+    range_1 -= 520;
+    if(range_1 == -2600){
+        range_1 = 0;
+    }
+}, 5000);
+
+//slider-2
+var range_2 = -520;
+var index = 2;
+var pre_index = 1;
+var prefix = "slider-bar-";
+setInterval(function(){
+    // picture
+    $(".slider-2").css("transform", "translate3d(" + range_2 + "px,0px,0px)");
+    range_2 -= 520;
+    if(range_2 == -3120){
+        range_2 = 0;
+    }
+    var id = prefix + index.toString();
+    $("#" + id).toggleClass("slider-bar-active");
+    index++;
+    if(index == 7){
+        index = 1;
+    }
+    // slider-bar
+    id = prefix + pre_index.toString();
+    $("#" + id).toggleClass("slider-bar-active"); 
+    pre_index++;
+    if(pre_index == 7){
+        pre_index = 1;
+    }
+
+    //counter
+    var count = index - 1;
+    if(count == 0){
+        count = 6;
+    }
+    $(".counter").text(count.toString() + "/6");
+}, 5500);
+
+// slider-3
+var range_3 = -67;
+setInterval(function(){
+    $(".slider-3").css("transform", "translate3d(0px," + range_3 + "px,0px)");
+    range_3 -= 67;
+    if(range_3 == -201){
+        range_3 = 0;
+    }
+}, 3000)
